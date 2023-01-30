ad47a1b314e3b7379581898bcc0b06ea60e68c1e
Enock MUKOKO MAYA <emukokomaya@gmail.com>
2023-01-30 17:35:14 +0100

    update admin dashboard

--- app-back/src/Controller/Admin/DashboardController.php
+++ app-back/src/Controller/Admin/DashboardController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Plantes;
 use App\Controller\Admin\UserCrudController;
 use Symfony\Component\HttpFoundation\Response;
+use App\Controller\Admin\PlantesCrudController;
+use App\Entity\User;
 use Symfony\Component\Routing\Annotation\Route;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -23,22 +27,36 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-    
+
         $routeBuilder = $this->get(AdminUrlGenerator::class);
 
-        return $this->redirect($routeBuilder->setController(UserCrudController::class)->generateUrl());
-
+        return $this->redirect($routeBuilder->setController(PlantesCrudController::class)->generateUrl());
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('App Back');
+            ->setTitle('Arosaje Admin');
     }
 
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::section('Plantes');
+        yield MenuItem::subMenu('Actions')
+            ->setSubItems([
+                MenuItem::linkToCrud('Voir les plantes', 'fas fa-eye', Plantes::class),
+                MenuItem::linkToCrud('Add Plantes', 'fas fa-plus', Plantes::class)
+                    ->setAction(Crud::PAGE_NEW),
+            ]);
+        yield MenuItem::section('Conseils');
+
+        yield MenuItem::subMenu('Actions')
+            ->setSubItems([
+                MenuItem::linkToCrud('Voir les conseils', 'fas fa-eye', Plantes::class),
+                MenuItem::linkToCrud('Add Plantes', 'fas fa-plus', Plantes::class)
+                    ->setAction(Crud::PAGE_NEW),
+            ]);
     }
 }
